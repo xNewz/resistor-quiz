22fb3f83bc0a4ec26e170aa4165a6b03478ed807
Pargorn Ruasijan <pargornruasijan@gmail.com>
2022-09-30 18:22:19 +0700

    add: project

---
+++ js/questions.js
@@ -0,0 +1,63 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "จากรูป ตัวต้านทานมีค่าเท่าไร?",
+    imgSrc: "/assets/bg1.png",
+    answer: "22MΩ 5%",
+    options: [
+      "10MΩ 5%",
+      "11MΩ 5%",
+      "20MΩ 5%",
+      "22MΩ 5%"
+    ]
+  },
+    {
+    numb: 2,
+    question: "จากรูป ตัวต้านทานมีค่าเท่าไร?",
+    imgSrc: "/assets/bg2.png",
+    answer: "10kΩ 5%",
+    options: [
+      "10kΩ 5%",
+      "20kΩ 5%",
+      "10Ω 10%",
+      "10MΩ 5%"
+    ]
+  },
+    {
+    numb: 3,
+    question: "จากรูป ตัวต้านทานมีค่าเท่าไร?",
+    imgSrc: "/assets/bg3.png",
+    answer: "3.5kΩ 5%",
+    options: [
+      "3.5Ω 5%",
+      "3.5kΩ 5%",
+      "3.5MΩ 5%",
+      "4.5kΩ 5%"
+    ]
+  },
+    {
+    numb: 4,
+    question: "จากรูป ตัวต้านทานมีค่าเท่าไร?",
+    imgSrc: "/assets/bg4.png",
+    answer: "100Ω 10%",
+    options: [
+      "100kΩ 5%",
+      "100MΩ 10%",
+      "100Ω 10%",
+      "1MΩ 5%"
+    ]
+  },
+    {
+    numb: 5,
+    question: "จากรูป ตัวต้านทานมีค่าเท่าไร?",
+    imgSrc: "/assets/bg5.png",
+    answer: "760Ω 10%",
+    options: [
+      "750Ω 10%",
+      "650Ω 10%",
+      "570Ω 10%",
+      "760Ω 10%"
+    ]
+  },
+];
